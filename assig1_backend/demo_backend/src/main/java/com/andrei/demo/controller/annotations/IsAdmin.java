1e3935cbbd76ab7a32e3b0d2d8b1afbfd2f9b53e
RaduTuodr <tudoreduard2004@gmail.com>
2026-04-20 10:19:59 +0300

    Added IsRole annotations for methods

---
+++ assig1_backend/demo_backend/src/main/java/com/andrei/demo/controller/annotations/IsAdmin.java
@@ -0,0 +1,14 @@
+package com.andrei.demo.controller.annotations;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasRole(T(com.andrei.demo.model.enums.RoleName).ADMIN.name())")
+public @interface IsAdmin {
+}
